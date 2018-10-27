3e4e883efbe9f213822a2f90934f3724ba97c4c1
pelan05 <pelan05@gmail.com>
2018-10-27 23:35:56 +0100

    .limpeza de codigo inutil

--- client/src/clientMain.c
+++ client/src/clientMain.c
@@ -88,13 +88,7 @@ int main(int argc, char *argv[], char* envp[]) {
 	//Variáveis
 	//
 
-	//int x, y;
-
-	//int i = 0, j = 0;
-	//int nrow, ncol, oposx, oposy; //num rows, num colunas, posx atual, posxy atual, old posx, old posy
 	int posx, posy;
-	//int ch; //character da keypress em numero int (ascii)
-	//char oldchar = ' ';
 	char * help; //para imprimir numeros nas linhas
 	char username[8] = {" "};
 
@@ -164,11 +158,6 @@ int main(int argc, char *argv[], char* envp[]) {
 	wrefresh(linhas);
 	wrefresh(nomes);
 	
-	/*posx = 1;//posiçao relativa à janela NOMES
-	posy = 1;
-
-	wmove(linhas, posx, posy);
-	refresh();*/
 					//imprimir numeros//not the prettiest solution but t'works
 					for(i = 0; i < 10; i++){
 						help = i+'0';
@@ -247,65 +236,9 @@ int main(int argc, char *argv[], char* envp[]) {
 	}while(choice != 27);//esc
 
 
-
-
-
-	// mexer asterisco pelo ecra - a funcionar - ~exemplo 2
-	/*
-
-	oldchar = mvwgetch(linhas, posx, posy);
-	
-	do{
-		ch = wgetch(linhas);
-		oposx = posx;
-		oposy = posy;
-
-		switch(ch){
-			case KEY_UP:
-
-				posy = (posy > 1)? posy - 1 : posy;
-				break;
-			case KEY_DOWN:
-				posy = (posy < (TAMJANY - 2))? posy + 1: posy;
-				break;
-			case KEY_LEFT:
-				posx = (posx > 1)? posx - 1: posx;
-				break;
-			case KEY_RIGHT:
-				posx = (posx < (TAMJANLINHASX - 2))? posx + 1: posx;
-				break;
-			case KEY_ENTER:
-				//TODO "Enter to edit line"
-				//posy é igual ao numero da linha
-				break;
-		}
-		if(ch == KEY_UP || ch == KEY_DOWN || ch == KEY_LEFT || ch == KEY_RIGHT){
-			
-			mvwaddch(linhas, oposy, oposx, ' ');
-			//oldchar = mvwgetch(linhas, posy, posx);
-			//attron(COLOR_PAIR(1));
-			mvwaddch(linhas, posy, posx, '*');//cursor atual
-			//attroff(COLOR_PAIR(1));
-			
-			////mvprintw(0, 0, "(%d, %d) ", posy, posx);
-			refresh();
-			wrefresh(linhas);
-			wrefresh(nomes);
-			for(i = 1; i < (TAMJANY-2)+1; i++){
-				help = i + '0'; //transforma i em char
-				mvwaddch(stdscr, i, TAMJANNOMESX, help);//ecra, y, x, char
-			}
-
-		}
-
-
-	}while(ch != KEY_EXIT);
-	*/
-
-
-
-
-
+	//
+	//endGame
+	//
 
 	endwin();//fechar ncurses control
 	return EXIT_SUCCESS;
