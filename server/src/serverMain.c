ea0e25fc940008d34a6f1ef9b529273600f1274a
Vascoide <vascoide650@hotmail.com>
2018-12-20 14:45:57 +0000

    ver 149 serverMain.c

--- server/src/serverMain.c
+++ server/src/serverMain.c
@@ -146,7 +146,7 @@ void* commandsThread(void* args){
 		}
 	
 
-
+		// not inside a circle, that's why it won't accept more than one command, I guess
 
 		return s; //isto cala o warning de nao haver return (void *);
 
@@ -398,7 +398,7 @@ void initSettings(settings * s, int argc, char * const argv[], char* envp[]) {
 	aux = NULL;
 	aux = getenv("MEDIT_MAXCOLUMNS");
 	if (aux != NULL) {
-		s->max_c = atoi(aux);
+		s->max_c = atoi(aux); 
 	}
 
 	aux = NULL;
